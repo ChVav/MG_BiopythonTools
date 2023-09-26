e415a8de4ad44f3cbd4935cada48d0e6c0916fef
ChVav <charlottevavourakis@gmail.com>
2023-09-26 10:56:22 +0200

    fixed subest_notin_list.py to output also to file

--- subset_notin_list.py
+++ subset_notin_list.py
@@ -4,6 +4,7 @@ def main():
     parser = argparse.ArgumentParser(description="Find items in a long list that are not in a subset.")
     parser.add_argument("--long-list-file", type=str, required=True, help="Input file containing the long list items, one item per line.")
     parser.add_argument("--subset-file", type=str, required=True, help="Input file containing the subset items, one item per line.")
+    parser.add_argument("--output-file", type=str, required=True, help="Output file to store the result.")
 
     args = parser.parse_args()
     
@@ -16,7 +17,9 @@ def main():
     subset_set = set(subset)
     not_in_subset = [item for item in long_list if item not in subset_set]
     
-    print(not_in_subset)
+    with open(args.output_file, "w") as output_file:
+        for item in not_in_subset:
+            output_file.write(str(item) + "\n")
 
 if __name__ == "__main__":
     main()
